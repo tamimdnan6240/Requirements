9f72216c2ae108481d6365d886304c8a3853f14f
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2023-03-14 18:00:55 -0400

    Update cuda_enabling.py

--- cuda_enabling.py
+++ cuda_enabling.py
@@ -1,12 +1,14 @@
-## enable cuda: 
-
 import torch 
-if torch.cuda.is_available():
-  device = torch.device("cuda")
-  torch.cuda.set_device(0)   ## set device id to use
+
+## enable cuda: 
+torch.device('cuda' if torch.cuda.is_available else 'cpu') 
+torch.cuda.set_device(0)   ## set device id to use
 
 ## check
 if torch.cuda.is_available():
   print("cuda is enabled")
 else:
   print("cuda is not enabled")
+  
+device = torch.device('cuda' if torch.cuda.is_available else 'cpu') 
+torch.cuda.is_available()
