61824160aa1094134715c4bde3158a8532968b11
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2024-05-29 14:15:26 -0400

    Create Packages.py

---
+++ Packages.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from scipy import stats
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+
+#for text pre-processing
+import re, string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+
+## For label encoding 
+from sklearn.preprocessing import LabelEncoder
+
+#for model-building
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+
+# bag of words
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+#for word embedding
+import gensim
+from gensim.models import Word2Vec
