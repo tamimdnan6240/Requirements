fea6ad3cadabae2d737a472547ad5d37f656a242
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2025-06-24 11:33:51 -0400

    Create import.py

---
+++ import.py
@@ -0,0 +1,27 @@
+# Data manipulation and preprocessing
+import pandas as pd
+import numpy as np
+
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pylab import *
+
+# Machine learning models and tools
+from sklearn import cluster, metrics
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.metrics import silhouette_score, calinski_harabasz_score
+from sklearn.model_selection import train_test_split
+
+# Hierarchical clustering
+import scipy.cluster.hierarchy as hca
+from scipy.cluster.hierarchy import dendrogram, linkage, cophenet
+from scipy.spatial.distance import pdist
+
+# Optional: Inline plotting for Jupyter Notebooks
+%matplotlib inline
+
+# Optional: Numpy printing config
+np.set_printoptions(suppress=True)
